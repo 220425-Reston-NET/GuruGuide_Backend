abf50a5ac4b5349a438f1b61c4479668dbc122a3
sgray23 <stevengray2311@gmail.com>
2022-06-30 15:05:31 -0400

    updated CoachRepository

--- GuruGuideDL/CoachesRepository.cs
+++ GuruGuideDL/CoachesRepository.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using GuruGuideModles;
 
 namespace GuruGuideDL
@@ -5,19 +6,42 @@ namespace GuruGuideDL
     public class CoachesRepository : IRepository<Coaches>
     {
         private string _filepath = "../GuruGuideDL/Data/Coaches.json";
-        public void Add(Coaches g_resource)
+        public void Add(Coaches g_coaches)
         {
-            throw new NotImplementedException();
+            List<Coaches> listofcoaches = GetAll();
+            listofcoaches.Add(g_coaches);
+
+            string jsonString = JsonSerializer.Serialize(listofcoaches, new JsonSerializerOptions{WriteIndented = true});
+            File.WriteAllText(_filepath, jsonString);
         }
 
         public List<Coaches> GetAll()
         {
-            throw new NotImplementedException();
+            string jsonString = File.ReadAllText(_filepath);
+            List<Coaches> listOfCoaches = JsonSerializer.Deserialize<List<Coaches>>(jsonString);
+
+            return listOfCoaches;
         }
 
-        public void Update(Coaches g_resource)
+        public void Update(Coaches g_Coaches)
         {
-            throw new NotImplementedException();
+             
+            List<Coaches> listOfCoaches = GetAll();
+
+            
+            foreach (Coaches coachObj in listOfCoaches)
+            {
+                
+                if (coachObj.CFullName == g_Coaches.CFullName)
+                {
+                    
+                    coachObj.ModalityOptions = g_Coaches.ModalityOptions;
+                }
+            }
+
+        
+            string jsonString = JsonSerializer.Serialize(listOfCoaches, new JsonSerializerOptions{WriteIndented = true});
+            File.WriteAllText(_filepath, jsonString);   
         }
     }
 }
