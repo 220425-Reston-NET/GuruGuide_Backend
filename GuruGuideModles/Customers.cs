9b5adbe68fbc3251b7f19d872113716d17cec36d
sgray23 <stevengray2311@gmail.com>
2022-07-01 10:45:29 -0400

    update customers

--- GuruGuideModles/Customers.cs
+++ GuruGuideModles/Customers.cs
@@ -4,31 +4,21 @@ namespace GuruGuideModles
 {
     public class Customers
     {
-        public string FullName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
-        public string Address {get; set; }
-        public string City {get; set;}
-        public string State { get; set; }
-        public string PhoneNumber {get; set;}
-        public string Gender {get; set;}
-        public string Ethnicity {get; set; }
-         public int Age {get; set;}
-        public string Username {get; set; }
-        public string Password {get; set; }
+        public string FirstName {get; set; }
+        public string LastName {get; set;}
+        public string Password { get; set; }
+        
 
         public Customers()
         {
-            FullName = "default";
+            UserName = "default";
             Email = "default";
-            Address = "default";
-            City = "default";
-            State = "default";
-            PhoneNumber = "default";
-            Gender = "default";
-            Ethnicity = "default";
-            Age = 0;
-            Username = "default";
+            FirstName = "default";
+            LastName = "default";
             Password = "default";
+            
         }
 
         
